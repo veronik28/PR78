ea7d92c158c8269034a6db907f8918779691082b
Veronika <157686128+veronik28@users.noreply.github.com>
2024-10-24 09:10:44 +0300

    Add files via upload

---
+++ Windows/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using WpfApp1.Windows;
+
+namespace WpfApp1.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonAddWeather_Click(object sender, RoutedEventArgs e)
+        {
+            AddWeatherWindow addWeatherWindow = new AddWeatherWindow();
+            addWeatherWindow.ShowDialog();
+        }
+        private void buttonShowWeather_Click(object sender, RoutedEventArgs e)
+        {
+            ShowWeatherWindow showWeatherWindow = new ShowWeatherWindow();
+            showWeatherWindow.ShowDialog();
+        }
+    }
+}
---
+++ Windows/ShowWeatherWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using WpfApp1.DataModel;
+using WpfApp1.CustomControls;
+
+namespace WpfApp1.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для ShowWeatherWindow.xaml
+    /// </summary>
+    public partial class ShowWeatherWindow : Window
+    {
+        public ShowWeatherWindow()
+        {
+            InitializeComponent();
+            LoadData();
+        }
+
+        private void LoadData() =>
+            new ModelEF().Prognosis.ToList().ForEach(item =>
+            wrapPanelWeather.Children.Add(new PrognosisWeather(item)));
+        private void buttonBack_Click(object sender, RoutedEventArgs e) => Close();
+    }
+}
